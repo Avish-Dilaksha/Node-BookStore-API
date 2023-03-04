917d717b4647d8dc3c689ada05b742232bff5768
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-04 12:41:14 +0530

    Created  error codes in errors  directory
    Created error codes by extending error class and implemented bad request, not found  in error diretcory for further use

---
+++ errors/index.js
@@ -0,0 +1,10 @@
+const customAPIError = require('./custom-api')
+const NotFoundError = require('./not-found')
+const BadRequestError = require('./bad-request')
+
+
+module.exports = {
+    customAPIError,
+    NotFoundError,
+    BadRequestError,
+}
