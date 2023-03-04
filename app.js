4933ef245e7b6aa94841f5f1dcf20c537ebea4da
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-04 12:12:57 +0530

    Created connect.js in db directory
    Created connect.js in db diretcory and implemented the code to connect mongoDB database

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const connectDB = require('./db/connect')
 
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.get('/', (req,res) => {
-    res.send('Hello world!')
-})
+// app.get('/', (req,res) => {
+//     res.send('Hello world!')
+// })
 
 const start = async () => {
     try {
-        // connect to DB (await)
+        // connect to DB
+        await connectDB(process.env.MONGO_URL)
         app.listen(port, () => {
             console.log(`Server is listening on ${port}...`);
         })
     } catch (error) {
-        console.log(er);
+        console.log(error);
     }
 }
 
